62e67d1c98b9303b22b785a119e28b87ec7dbbe4
MGome <migomez6@uc.cl>
2021-10-19 22:08:24 -0300

    cambios en el middleware

--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,29 @@
 var express = require('express');
 var MD5 = require("md5");
+var axios = require("axios");
+const jwt = require("jsonwebtoken");
 var router = express.Router();
+var { getCookie } = require('../middlewares/auth');
 
-
+router.use(getCookie);
 /* GET users listing. */
-router.post('/', async (req, res, next) => {
-  // res.send('respond with a resource');
-  var formBody = req.body;
-  
-  formBody['password'] = MD5(formBody['password']).toString();
-  formBody['http_api_version'] = '3.4.0';
+router.get('', async (req, res, next) => {
+  if (res.locals.error) {
+    return;
+  }
+  var token;
+  var respuesta = await axios.get("https://www.google.com/", {withCredentials: true})
+  .then(function (response) {
+    
+    var separado = response.headers['set-cookie'][0].split(";");
+    var cookieData = separado[0].split("=");
+    console.log(cookieData[1]);
+    token = jwt.sign({user: req.data.username, key: cookieData[0], value: cookieData[1]}, process.env.HASH_KEY,{expiresIn: "1h",});
+    return response.data;
+  });
 
-  var formBody = new URLSearchParams(formBody).toString();
-  var r_headers;
-  var response = await fetch(
-    `${process.env.URL_API}/EHCommon/ModularAdmin/UserAPI/login`, 
-  {method: 'POST',
-  credentials: 'include', 
-  headers: 
-  {'Content-Type': 'application/x-www-form-urlencoded'},
-  'body': formBody }).then(function(response){
-    r_headers = response.headers.raw()['set-cookie'];
-    return response.json()
-  }).catch(r => console.log(r));
-
-  var separado = r_headers[0].split(";");
-  var cookieData = separado[0].split("=");
-  res.cookies.set(cookieData[0], cookieData[1], { secure: true, signed: false, sameSite: "none" });
-  res.cookies.set(cookieData[0]+".sig");
-  res.status = 201
-  res.body = response
+  res.set('token', token);
+  res.send(respuesta);
 });
 
 module.exports = router;
